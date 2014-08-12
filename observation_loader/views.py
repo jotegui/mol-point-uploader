18ac1336f87c8a19ec210e20f79c202d6bdc45d1
Javier Otegui <javier.otegui@gmail.com>
2014-08-12 16:51:00 -0600

    Added session; Basic alignment of headers

--- observation_loader/views.py
+++ observation_loader/views.py
@@ -1,6 +1,7 @@
 from observation_loader import app
+import uuid
 
-from flask import render_template, redirect, url_for, request, send_from_directory, flash
+from flask import render_template, redirect, url_for, request, send_from_directory, flash, session
 from werkzeug.utils import secure_filename
 
 
@@ -24,17 +25,36 @@ def download_spreadsheet():
 # File upload
 @app.route('/upload', methods=['GET','POST'])
 def upload():
-    
+
     up_file = request.files['file']
+    
+    # TODO: Validate file name and extension
+    # TODO: Validate ',' or '\t' separated and store in session
     
     if "useTemplate" in request.form:
         pass # Template used
     
-    headers = up_file.readline().split(",")
-    return render_template("/headers.html", headers=headers)
+    file_uuid = uuid.uuid4()
+    session['file_uuid'] = file_uuid
+    session['file_headers'] = up_file.readline().split(",")
+    session['file_content'] = up_file.read()
+    
+    return render_template("/headers.html")
 
 
 # Metadata about the fields
 @app.route('/metafields', methods=['GET','POST'])
 def metafields():
+    alignment = {
+        "scientificName": request.form['scientificName'],
+        "latitude": request.form['latitude'],
+        "longitude": request.form['longitude'],
+        "observationDate": request.form['observationDate'],
+        "collector": request.form['collector']
+    }
+    
+    # TODO: Validate content of required headers (above)
+    # TODO: Prepare new page for metadata about other fields
+    # TODO: Then, redirect to metadata about the project
+    
     return render_template("metafields.html")
