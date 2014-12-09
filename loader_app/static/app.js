2a4c25819ea8bcdef45bf22f1108a91f1c2a594d
Javier Otegui <javier.otegui@gmail.com>
2014-12-09 14:47:04 +0100

    Issue: Default value for eventDate is mandatory: fixed

--- loader_app/static/app.js
+++ loader_app/static/app.js
@@ -54,11 +54,13 @@
             if (key=="eventDate") {
                 var regexIso8601 = /^(\d{4}|\+\d{6})(?:-(\d{2})(?:-(\d{2})(?:T(\d{2}):(\d{2}):(\d{2})\.(\d{1,})(Z|([\-+])(\d{2}):(\d{2}))?)?)?)?$/;
                 var match;
-                if (typeof this.headers[key+"Default"] === "string" && (match = this.headers[key+"Default"].match(regexIso8601))) {
-                    console.log("date matches ISO");
-                } else {
-                    if (this.warning=="") {
-                        this.warning = "Warning: default value for eventDate is not a valid date";
+                if (this.headers[key] == "") {
+                    if (typeof this.headers[key+"Default"] === "string" && (match = this.headers[key+"Default"].match(regexIso8601))) {
+                        console.log("date matches ISO");
+                    } else {
+                        if (this.warning=="") {
+                            this.warning = "Warning: default value for eventDate is not a valid date";
+                        }
                     }
                 }
             }
